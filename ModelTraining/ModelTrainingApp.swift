2e4743a13fee11e558c5e68e376415b77bdb1b98
Kyle <kylei.tw@gmail.com>
2025-06-03 16:33:12 +0800

    Initial Commit

---
+++ ModelTraining/ModelTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelTrainingApp.swift
+//  ModelTraining
+//
+//  Created by admin on 2025/6/3.
+//
+
+import SwiftUI
+
+@main
+struct ModelTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
